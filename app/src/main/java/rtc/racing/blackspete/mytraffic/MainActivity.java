b103102f0a42f94dc08925badc33e8d71fe5f84e
blackspete <iblackspete@gmail.com>
2015-12-05 14:29:44 +0700

    button controller finish

--- app/src/main/java/rtc/racing/blackspete/mytraffic/MainActivity.java
+++ app/src/main/java/rtc/racing/blackspete/mytraffic/MainActivity.java
@@ -1,13 +1,56 @@
 package rtc.racing.blackspete.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutmeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    }   // Main Method
+
+    private void buttonController() {
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.cow);
+                buttonMediaPlayer.start();
+
+            }   // event
+        });
     }
-}
+
+    private void bindWidget() {
+
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (Button) findViewById(R.id.button);
+
+
+
+
+
+
+
+    }
+}   // Main Class
