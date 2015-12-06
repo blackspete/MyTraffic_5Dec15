20b8e800f49a72bc2f8f534d7097ea678410ccc6
blackspete <iblackspete@gmail.com>
2015-12-06 14:01:42 +0700

    detail activity ใกล้เสดแล้วนะ

---
+++ app/src/main/java/rtc/racing/blackspete/mytraffic/DetailActivity.java
@@ -0,0 +1,44 @@
+package rtc.racing.blackspete.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    // explicit
+    private TextView titleTextview,detailTextview;
+    private ImageView trafficImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //bindwidget
+        bindWidget();
+        //Show view
+        showView();
+
+    } // main medthod
+
+    private void showView() {
+        //การรับค่า จาก intent
+        String strTitle = getIntent().getStringExtra("Title");
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        titleTextview.setText(strTitle); //เอามาโชว์
+        trafficImageView.setImageResource(intIcon);
+
+    }
+
+    private void bindWidget() {
+        titleTextview = (TextView) findViewById(R.id.textView4);
+        detailTextview = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+    }
+}// main class
--- app/src/main/java/rtc/racing/blackspete/mytraffic/MainActivity.java
+++ app/src/main/java/rtc/racing/blackspete/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void listViewController() {
         //For Icon
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
                 R.drawable.traffic_03, R.drawable.traffic_04,
                 R.drawable.traffic_05, R.drawable.traffic_06,
                 R.drawable.traffic_07, R.drawable.traffic_08,
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_19, R.drawable.traffic_20};
 
         // for title
-        String[] titleStrings = new String[20];
+        final String[] titleStrings = new String[20];
         titleStrings[0] = "หัวข้อที่ 1";
         titleStrings[1] = "หัวข้อที่ 2";
         titleStrings[2] = "หัวข้อที่ 3";
@@ -71,6 +72,19 @@ public class MainActivity extends AppCompatActivity {
         String[] detailStrings = getResources().getStringArray(R.array.detail_short);
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, titleStrings, detailStrings, intIcon);
         trafficListView.setAdapter(objMyAdapter);
+
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //intent เคลื่อนย้ายการทำงาน จาก detail ไป main
+                Intent objIntent = new Intent(MainActivity.this,DetailActivity.class);
+                objIntent.putExtra("Title", titleStrings[position]);
+                objIntent.putExtra("Image", intIcon[position]);
+                objIntent.putExtra("Index", position);
+                startActivity(objIntent);
+            }
+        });
+
     } //listviewcontroller
 
     private void buttonController() {
